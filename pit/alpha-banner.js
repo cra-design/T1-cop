d962788518a3d4436c87ab20a28373438c8e615a
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2025-02-05 14:37:48 -0700

    Update alpha-banner.js

--- pit/alpha-banner.js
+++ pit/alpha-banner.js
@@ -1,7 +1,7 @@
 // if you want the banner off, just comment out the file
 
 var alphaBanner = document.getElementsByTagName('BODY')[0];
-alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha-top bg-warning mrgn-tp-0 mrgn-bttm-0" role="banner"><div class="container"><p class="h4 mrgn-bttm-lg mrgn-tp-md">This site is for CRA testing only</p></div></section >');
+alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha-top bg-warning mrgn-tp-0 mrgn-bttm-0" role="banner"><div class="container"><p class="h2">This site is for CRA testing only</p></div></section >');
 
 
 //Renders search disabled
