8d15816d929a5655e4f685bfe4d4acf3995f035c
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2025-02-06 14:10:12 -0700

    Update alpha-banner.js

--- pit/alpha-banner.js
+++ pit/alpha-banner.js
@@ -9,7 +9,7 @@ $('#wb-srch-sub').attr("disabled", true);
 
 //Separate script to null all canada.ca links to a 404 page (including GC menu) and redirect some in menu
 $( ".gcweb-menu" ).on( "wb-ready.gcweb-menu", function( event ) {
-	//$("a[href='https://www.canada.ca/en/revenue-agency/services/e-services/cra-login-services.html']").attr("href", "cra-login-services.html").addClass("prototype");
+	$("a[href$='cra-login-services.html']").attr("href", "cra-login-services.html").addClass("prototype");
 	//$("a[href='https://www.canada.ca/en/services/taxes/income-tax.html']").attr("href", "https://cra-proto.github.io/sign-in-test/en/services/taxes/income-tax.html").addClass("prototype");
 	$("main a").not("[href*='/pit/']").attr("href", "404.html").addClass("test1");
 	$(".gcweb-menu a:not(.prototype)").attr("href", "404.html").addClass("test2");	
