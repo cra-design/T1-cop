26455584f5c3e141557b1a2e1bdaaf154c3b5235
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2025-02-06 13:52:35 -0700

    Update alpha-banner.js

--- pit/alpha-banner.js
+++ pit/alpha-banner.js
@@ -12,16 +12,16 @@ $( ".gcweb-menu" ).on( "wb-ready.gcweb-menu", function( event ) {
 	$("a[href='https://www.canada.ca/en/revenue-agency/services/e-services/cra-login-services.html']").attr("href", "cra-login-services.html").addClass("prototype");
 	//$("a[href='https://www.canada.ca/en/services/taxes/income-tax.html']").attr("href", "https://cra-proto.github.io/sign-in-test/en/services/taxes/income-tax.html").addClass("prototype");
 	
-	$(".gcweb-menu a:not(.prototype)").attr("href", "404.html");	
-	$("a[href*='canada.ca']").attr("href", "404.html").addClass("test");
+	$(".gcweb-menu a:not(.prototype)").attr("href", "404.html").addClass("test1");	
+	$("a[href*='canada.ca']").attr("href", "404.html").addClass("test2");
 	$("a[href*='.gc.ca/']").attr("href", "404.html");
-	$("main a").not("[href*='/pit/']").attr("href", "404.html");
+	
 });
 
 //Separate script to null all canada.ca links to a 404 page
 $( document ).on( "wb-ready.wb", function( event ) {
-	
-	$("a[href*='canada.ca']").attr("href", "404.html").addClass("test2");
+	$("main a").not("[href*='/pit/']").attr("href", "404.html");
+	$("a[href*='canada.ca']").attr("href", "404.html").addClass("test3");
 $("a[href*='.gc.ca/']").attr("href", "404.html");
 $("#chat-bottom-bar, #gc-pft, .pagedetails details").addClass("hidden");	
 });
